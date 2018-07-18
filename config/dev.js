c4bb7c22e74b1e8552e5f3ea20b2f2be832fa3ca
Zaknefeinn <zaknefeinn@gmail.com>
2018-07-18 16:49:49 -0500

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb://Zaknefeinn:1234asdf@ds141671.mlab.com:41671/bill-planner',
+  secretOrKey: 'secret'
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('connected'))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+app.use('/api/users', users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
