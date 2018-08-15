f5f2c5a37607c17e8029424c9b79a5bda7a87dda
Zaknefeinn <zaknefeinn@gmail.com>
2018-08-15 16:57:28 -0500

    end of day

--- client/src/components/Dashboard/Account/Account.js
+++ client/src/components/Dashboard/Account/Account.js
@@ -7,11 +7,32 @@ class Account extends Component {
       <div className="Account">
         <div className="account-container">
           <div className="account-title">Accounts</div>
+          <div className="account-add">
+            <span className="fas fa-plus" />
+          </div>
+          <div className="account-add-container">
+            <div className="account-add-sub">
+              <span>Account</span>
+              <input type="text" />
+            </div>
+            <div className="account-add-sub">
+              <span>Balance</span>
+              <input type="text" />
+            </div>
+          </div>
           <table className="account-list">
-            <tr>
-              <th>Account Name</th>
-              <th>Balance</th>
-            </tr>
+            <thead>
+              <tr>
+                <th>Account Name</th>
+                <th>Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Arvest</td>
+                <td>$1205</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
